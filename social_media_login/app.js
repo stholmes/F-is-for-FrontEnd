2c00f0f8056e63550347dd05f227d26e7195c049
stholmes <stephenshigemi@gmail.com>
2020-03-15 13:48:37 -0700

    Create event listeners for registration inputs

--- social_media_login/app.js
+++ social_media_login/app.js
@@ -1,32 +1,74 @@
 window.onload = function(){
 
-    let inputValidation = (event, defaultValue) =>{
-        if(event.target.value ==''){
-            event.target.value = defaultValue;
-        }
-        console.log('validation')
-    }
-    var logIn = document.getElementById("log-in");
-    logIn.addEventListener("click", function(){
+   let logIn = document.getElementById("log-in");
+    logIn.addEventListener("click", ()=>{
        let validation = document.querySelectorAll(".validation");
        for (let i = 0 ; i < validation.length; i++){
            validation[i].style.display="block"
        }
     })
-    var fName = document.getElementById('firstName');
-    fName.addEventListener("focus", function(event){
-        if(fName.value == 'First Name'){
-            event.target.value='';
+    //Registration event listeners
+    let fName = document.getElementById('firstName');
+    let lname = document.getElementById('lastName');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    fName.addEventListener('focus', (event)=>{
+    if(event.target.value == 'First Name'){
+            event.target.value ='';
         }
     })
-    fName.addEventListener("blur", function(event){
-        if(event.target.value==''){
-            event.target.value='First Name'
-        }
-        if(event.target.value.length>0 && event.target.value.trim().length==0){
-            event.target.value='First Name'
+    fName.addEventListener('blur', (event)=>{
+        let text = event.target.value;
+        if(text==''){
+            text='First Name'
+        }else if(text.length>0 && text.trim().length==0){
+            text='First Name'
         } 
-    });
+        event.target.value = text;
+    })
+
+    lname.addEventListener('focus', (event)=>{
+        if(event.target.value == 'Last Name'){
+            event.target.value ='';
+        }
+    })
+    lname.addEventListener('blur', (event)=>{
+        let text = event.target.value;
+        if(text==''){
+            text='Last Name'
+        }else if(text.length>0 && text.trim().length==0){
+            text='Last Name'
+        } 
+        event.target.value = text;
+    })
+    email.addEventListener('focus', (event)=>{
+        if(event.target.value == 'Email'){
+            event.target.value ='';
+        }
+    })
+    email.addEventListener('blur', (event)=>{
+        let text = event.target.value;
+        if(text==''){
+            text='Email'
+        }else if(text.length>0 && text.trim().length==0){
+            text='Email'
+        } 
+        event.target.value = text;
+    })
+    password.addEventListener('focus', (event)=>{
+        if(event.target.value == 'Password'){
+            event.target.value ='';
+        }
+    })
+    password.addEventListener('blur', (event)=>{
+        let text = event.target.value;
+        if(text==''){
+            text='Password'
+        }else if(text.length>0 && text.trim().length==0){
+            text='Password'
+        } 
+        event.target.value = text;
+    })
 
 
 }
